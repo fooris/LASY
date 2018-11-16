1721935770e9dda722b0bd1b572209dff080b352
Sebastian Kappes <shk@ThinkPadX220>
2018-11-17 00:40:01 +0100

    move ffmpeg stuff to extra class

---
+++ src/FFMPEG.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+
+public class FFMPEG {
+    private static final String FFMPEG_PATH = "/usr/bin/ffmpeg";
+
+    public static String convertToAudioAndGetPath(String inputFilePath) {
+        String tmpDir = System.getProperty("java.io.tmpdir");
+        String outFilePath = tmpDir + "/audio.wav";
+        String ffmpegCommand = FFMPEG_PATH + " -i " + " " + inputFilePath + " -ac 1 -sample_rate 16000 -y " + outFilePath;
+        System.out.println(ffmpegCommand);
+        try {
+            Runtime.getRuntime().exec(ffmpegCommand);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return outFilePath;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,11 @@ import java.io.IOException;
 
 public class Main {
 
-    private static final String FFMPEG_PATH = "/usr/bin/ffmpeg";
 
     public static void main(String args[]) {
 
-        convertToAudioAndGetPath(args[0]);
+        FFMPEG.convertToAudioAndGetPath(args[0]);
         System.out.println(args[0] );
     }
 
-    static String convertToAudioAndGetPath(String inputFilePath) {
-        String tmpDir = System.getProperty("java.io.tmpdir");
-        String outFilePath = tmpDir + "/audio.wav";
-        String ffmpegCommand = FFMPEG_PATH + " -i " + " " + inputFilePath + " -ac 1 -sample_rate 16000 -y " + outFilePath;
-        System.out.println(ffmpegCommand);
-        try {
-            Runtime.getRuntime().exec(ffmpegCommand);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return outFilePath;
-    }
 }
