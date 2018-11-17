b3b6a69ccc207cfb3f8ef5369a2e2ca5686a14fb
Matthias <Matthias>
2018-11-17 04:43:19 +0100

    Ben went for a walk with his dog!

--- src/main/java/SoundLoader.java
+++ src/main/java/SoundLoader.java
@@ -9,40 +9,6 @@ import java.util.List;
 public class SoundLoader {
 
 
-    public static final int SAMPLE_RATE = 16000;
-    public static final int REDUCTION_FACTOR = 50;
-
-
-    public static void main(String[] args) throws UnsupportedAudioFileException {
-
-        double[] sound = read("./temp/audio.wav");
-
-        sound = reduce(sound, REDUCTION_FACTOR);
-        int kSize = (int) (1.5 * (SAMPLE_RATE / REDUCTION_FACTOR));
-        GaussFilter filter = new GaussFilter(kSize / 6, kSize);
-        sound = filter.filter(sound);
-
-        List<Interval> fupelList = SilenceDetector.detectSilence(sound, 0.1, (SAMPLE_RATE / REDUCTION_FACTOR) / 2);
-
-        double secondsSaved = fupelList.stream()
-                .map(i -> i.getTimeEnd() - i.getTimeStart())
-                .reduce(Double::sum)
-                .orElse(0.0);
-
-        double maxSecondsSaved = fupelList.stream()
-                .map(i -> i.getTimeEnd() - i.getTimeStart())
-                .reduce(Double::max)
-                .orElse(0.0);
-
-        double avgSecondsSaved = secondsSaved / fupelList.size();
-
-        System.out.println("Number of cuts: " + fupelList.size());
-        System.out.printf("Avg seconds saved/cut: %.2f\n", avgSecondsSaved);
-        System.out.printf("Max seconds saved/cut: %.2f\n", maxSecondsSaved);
-        System.out.printf("Seconds saved: %.2f\n", secondsSaved);
-        System.out.printf("Percent saved: %.2f\n", 100.0 * secondsSaved / (sound.length / (SoundLoader.SAMPLE_RATE / REDUCTION_FACTOR)));
-
-    }
 
     private static double[] reduce(double[] orig, int reductionFactor) {
         double[] reduced = new double[orig.length / reductionFactor];
