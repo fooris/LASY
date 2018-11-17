f88b1c4fe508f2454e42e75fef39095494a8f33d
Felix Fischer <felix.ma.fischer@gmail.com>
2018-11-17 08:47:43 +0000

    cleaned up silence detector

--- src/main/java/SilenceDetector.java
+++ src/main/java/SilenceDetector.java
@@ -7,33 +7,34 @@ public class SilenceDetector {
     private String fileName;
     private double threshold_mult;
     private int min_sample_length;
-    private int soundLength;
-    public SilenceDetector(String fileName, double threshold_mult, int min_sample_length ){
+    private double[] rawSound;
+
+    public SilenceDetector(String fileName, double threshold_mult, int min_sample_length) {
         this.fileName = fileName;
+        try {
+            this.rawSound = SoundLoader.read(fileName);
+        } catch (UnsupportedAudioFileException e) {
+            e.printStackTrace();
+        }
         this.threshold_mult = threshold_mult;
         this.min_sample_length = min_sample_length;
-
     }
 
     public List<Interval> detectSilence() throws UnsupportedAudioFileException {
 
-
-        double[] sound = SoundLoader.read(fileName );
-
-
-        sound = SoundLoader.reduce(sound, SoundLoader.REDUCTION_FACTOR);
+        double[] reducedSound = SoundLoader.reduce(this.rawSound, SoundLoader.REDUCTION_FACTOR);
         int kSize = (int) (1.5 * (SoundLoader.SAMPLE_RATE / SoundLoader.REDUCTION_FACTOR));
-        GaussFilter filter = new GaussFilter(kSize / 6, kSize);
-        sound = filter.filter(sound);
+        Filter filter = new GaussFilter(kSize / 6, kSize);
+        double[] filteredSound = filter.filter(reducedSound);
 
         double max = 0;
-        for (double v : sound) max = Double.max(max, Math.abs(v));
+        for (double v : filteredSound) max = Double.max(max, Math.abs(v));
 
         boolean currentlySilent;
         int startSample = -1;
         List<Interval> fupelList = new ArrayList<>();
-        for (int i = 1; i < sound.length; i++) {
-            currentlySilent = Math.abs(sound[i]) < threshold_mult * max;
+        for (int i = 1; i < filteredSound.length; i++) {
+            currentlySilent = Math.abs(filteredSound[i]) < threshold_mult * max;
             // silence star
             if (startSample == -1 & currentlySilent) {
                 startSample = i;
@@ -45,13 +46,10 @@ public class SilenceDetector {
             }
         }
 
-        soundLength = sound.length;
         return fupelList;
     }
 
-
-
-    public void report(List<Interval> fupelList){
+    public void report(List<Interval> fupelList) {
         double secondsSaved = fupelList.stream()
                 .map(i -> i.getTimeEnd() - i.getTimeStart())
                 .reduce(Double::sum)
@@ -64,11 +62,11 @@ public class SilenceDetector {
 
         double avgSecondsSaved = secondsSaved / fupelList.size();
 
-
         System.out.println("Number of cuts: " + fupelList.size());
         System.out.printf("Avg seconds saved/cut: %.2f\n", avgSecondsSaved);
         System.out.printf("Max seconds saved/cut: %.2f\n", maxSecondsSaved);
         System.out.printf("Seconds saved: %.2f\n", secondsSaved);
-        System.out.printf("Percent saved: %.2f\n", 100.0 * secondsSaved / (soundLength / (SoundLoader.SAMPLE_RATE / SoundLoader.REDUCTION_FACTOR)));
+        System.out.printf("Percent saved: %.2f\n", 100.0 * secondsSaved / (rawSound.length / SoundLoader.SAMPLE_RATE));
+
     }
 }
