84eec23a009d686a1abfe474d9e2ccd98b831efd
Ben <be.loehner@gmail.com>
2018-11-17 11:02:14 +0100

    Add VideoSyncer

---
+++ src/main/java/TestMainVideo.java
@@ -0,0 +1,20 @@
+import javax.sound.sampled.UnsupportedAudioFileException;
+import java.util.List;
+
+public class TestMainVideo {
+
+
+    public static void main(String[] args) throws UnsupportedAudioFileException {
+
+        SilenceDetector sl = new SilenceDetector("./temp/audio.wav", 0.1, 0.5);
+        List<Interval> fupelList = sl.detectNotSilence();
+        sl.report(fupelList);
+
+        double[] newSound = TestCutter.cut(fupelList, sl.getRawSound());
+
+        SoundLoader.save("./temp/test.wav", newSound);
+
+    }
+
+
+}
