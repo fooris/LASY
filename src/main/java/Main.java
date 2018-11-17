224035fde96ddf2b65d82a0ba06f785e6754dc53
Matthias <Matthias>
2018-11-17 09:57:37 +0100

    Ben also became angry

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,20 @@
+import javax.sound.sampled.UnsupportedAudioFileException;
 import java.io.IOException;
+import java.util.List;
 
 public class Main {
 
 
-    public static void main(String args[]) {
+    public static void main(String args[]) throws UnsupportedAudioFileException {
 
-        FFMPEG.convertToAudioAndGetPath(args[0]);
-        System.out.println(args[0] );
+        String audio = FFMPEG.convertToAudioAndGetPath(args[0]);
+        double thres = 0.1;
+        if(args.length >1){
+            thres = Double.parseDouble(args[1]);
+        }
+        SilenceDetector sl =  new SilenceDetector(audio, thres, (SoundLoader.SAMPLE_RATE / SoundLoader.REDUCTION_FACTOR) / 2);
+        List<Interval> fupelList = sl.detectSilence();
+        sl.report(fupelList);
     }
 
 }
